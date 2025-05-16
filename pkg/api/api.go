c783e9e5a333531dd41b7a433dd2adcdc99bd142
David Pello <dvpg@tuta.io>
2025-05-16 10:35:07 +0200

    GerTupper method and new test tupper 5

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,29 +1,38 @@
 package api
 
-import "errors"
+import (
+	"errors"
 
-type Tupper struct {
-	ID    string
-	Model int
-}
+	"github.com/ladecadence/EcoBoxGUI/pkg/models"
+)
 
-type User struct {
-	ID      string
-	Name    string
-	Surname string
-	Tuppers []Tupper
-}
-
-var testUsers = []User{
-	{ID: "d343b80c-ae82-41f5-ad77-7a32d1be85e2", Name: "Perico", Surname: "De los palotes", Tuppers: []Tupper{{ID: "tuper1", Model: 0}}},
+var testUsers = []models.User{
+	{ID: "d343b80c-ae82-41f5-ad77-7a32d1be85e2", Name: "Perico", Surname: "De los palotes", Tuppers: []models.Tupper{{ID: "tuper1", Number: 5}}},
 	{ID: "898ceaf8-2b51-4a4b-8055-d04384620dc9", Name: "Manolo", Surname: "El del bombo"},
 }
 
-func GetUser(id string) (User, error) {
+var testTuppers = []models.Tupper{
+	{ID: "e28069150000501d63e8f8e4", Number: 1},
+	{ID: "e28069150000501d63e900e4", Number: 2},
+	{ID: "e28069150000401d63e904e4", Number: 3},
+	{ID: "e28069150000401d63e8fce4", Number: 4},
+	{ID: "e28069150000501d63e8f4e4", Number: 5},
+}
+
+func GetUser(id string) (models.User, error) {
 	for _, u := range testUsers {
 		if u.ID == id {
 			return u, nil
 		}
 	}
-	return User{}, errors.New("no such user")
+	return models.User{}, errors.New("no such user")
+}
+
+func GetTupper(id string) (models.Tupper, error) {
+	for _, t := range testTuppers {
+		if t.ID == id {
+			return t, nil
+		}
+	}
+	return models.Tupper{}, errors.New("no such tupper")
 }
