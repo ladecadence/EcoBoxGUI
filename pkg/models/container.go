d957a1dde17d5f213dcee62b26ce871d98f42900
David Pello <dvpg@tuta.io>
2025-10-03 11:03:11 +0200

    Add available field to Container model

--- pkg/models/container.go
+++ pkg/models/container.go
@@ -1,12 +1,13 @@
 package models
 
 type Container struct {
-	Code   string `json:"codigo"`
-	NameES string `json:"nombre_es"`
-	NameEU string `json:"nombre_eu"`
-	Active int    `json:"active"`
-	Icon   string `json:"icono"`
-	Price  string `json:"precio"`
+	Code      string `json:"codigo"`
+	NameES    string `json:"nombre_es"`
+	NameEU    string `json:"nombre_eu"`
+	Active    int    `json:"activo"`
+	Available bool   `json:"disponible"`
+	Icon      string `json:"icono"`
+	Price     string `json:"precio"`
 }
 
 type Containers struct {
